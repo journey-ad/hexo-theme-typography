b4cdf1f4776580ba0ef1e4b0b52f6ba9310c4a68
journey-ad <journey.adc@gmail.com>
2020-01-13 02:34:40 +0800

    update cplayer

--- scripts/lib/cplayer-tag.js
+++ scripts/lib/cplayer-tag.js
@@ -14,10 +14,10 @@ module.exports = function (args, contents) {
 
   args.forEach((arg) => {
     switch (arg.trim()) {
-      case "autoplay": 
+      case "autoplay":
         autoplay = true;
         break;
-      case "yaml": 
+      case "yaml":
         parser = 'yaml';
         break;
       case "cson":
@@ -44,12 +44,12 @@ module.exports = function (args, contents) {
 
   let playlist = parse(contents, parser);
 
-  let resPlaylist = [{name: 'loading...', artist: 'loading...'}];
+  let resPlaylist = [{ name: 'loading...', artist: 'loading...' }];
   let add163 = [];
 
-  playlist = playlist.forEach(function(v) {
+  playlist = playlist.forEach(function (v) {
     switch (typeof v) {
-      case 'number': 
+      case 'number':
         add163.push(v);
         break;
       default:
@@ -142,15 +142,54 @@ module.exports = function (args, contents) {
                         width: 100%;
                         margin: 10px auto;
                       }
+                      c-player .cp-mainbody{
+                        min-height: 5.16667em;
+                      }
+                      c-player .cp-poster{
+                        width: 5.16667em;
+                        height: 5.16667em;
+                      }
+                      c-player .cp-center-container{
+                        height: 5.16667em;
+                      }
+                      c-player .cp-play-button, c-player .cp-volume-button, c-player .cp-prev-button, c-player .cp-list-button, c-player .cp-mode-button, c-player .cp-next-button{
+                        width: 1.54167em;
+                        height: 1.54167em;
+                      }
+                      c-player .cp-icon, c-player .cp-prev-icon, c-player .cp-next-icon, c-player .cp-volume-icon, c-player .cp-random-icon, c-player .cp-single-icon, c-player .cp-loop-icon, c-player .cp-list-icon{
+                        height: 1.54167em;
+                        width: 1.54167em;
+                      }
+                      c-player .cp-play-button{
+                        height: 2.8em;
+                        width: 2.8em;
+                      }
+                      c-player .cp-play-button .cp-play-icon.cp-play-icon-paused {
+                        margin-left: 1em;
+                      }
+                      c-player .cp-play-button .cp-play-icon.cp-play-icon-paused {
+                        margin: 0.7875em;
+                      }
+                      c-player .cp-volume-icon {
+                        width: 1.76667em;
+                      }
+                      c-player .cp-list-button {
+                        width: 1.54167em;
+                        height: 1.54167em;
+                      }
+                      c-player .cp-mode-button {
+                        width: 1.54167em;
+                        height: 1.54167em;
+                      }
                     }
                     \`
           });
 
           ${
-            add163.map((a) => {
-              return `window.cplayerList[${JSON.stringify(targetID)}].add163(${JSON.stringify(a)})`
-            }) || ''
-          }
+    add163.map((a) => {
+      return `window.cplayerList[${JSON.stringify(targetID)}].add163(${JSON.stringify(a)})`
+    }) || ''
+    }
 
           let player = window.cplayerList[${JSON.stringify(targetID)}]
           player.to(1)
